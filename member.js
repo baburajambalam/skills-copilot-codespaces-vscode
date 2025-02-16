91dba8d7ef369b35eda7ef67e86f1bcc4f80ec84
Baburaj <baburaj.ambalam@gmail.com>
2025-02-16 14:47:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['JS', 'HTML', 'CSS'],
+        greet: function(){
+            console.log('Hello');
+        }
+    }
+    return member;
+}
